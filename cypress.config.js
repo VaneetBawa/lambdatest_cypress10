b7e1965d496cbf8ff40b2f5429c5937de088b8fe
VaneetBawa <37815424+VaneetBawa@users.noreply.github.com>
2025-03-20 22:27:36 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-  requestTimeout: 360000,
-  responseTimeout: 360000,
-  pageLoadTimeout: 360000
+      // Implement node event listeners here
     },
+    requestTimeout: 360000,
+    responseTimeout: 360000,
+    pageLoadTimeout: 360000
   },
 });
